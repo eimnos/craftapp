be556e3c5aee98b74ecd523b5b196a86b3002717
eimnos <sblasi.mail@gmail.com>
2024-12-06 12:50:53 +0100

    Aggiornamento Toolbar

---
+++ src/components/Toolbar.js
@@ -0,0 +1,23 @@
+import { Box, Button } from "@mui/material";
+
+function Toolbar({ buttons }) {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "flex-end",
+        alignItems: "center",
+        marginBottom: 2,
+        padding: 2,
+      }}
+    >
+      {buttons.map((button, index) => (
+        <Button key={index} variant="contained" onClick={button.onClick} sx={{ mr: 1 }}> 
+          {button.label}
+        </Button>
+      ))}
+    </Box>
+  );
+}
+
+export default Toolbar;
